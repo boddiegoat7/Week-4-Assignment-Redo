23d1c76571dedbc7ce33aaf2017559343e1b2822
victor boddie <victorboddie21@gmail.com>
2022-10-25 14:46:18 -0400

    added css and fixed html

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,124 +8,114 @@ var questionAnswerCorrect;
 var selectionArray = [];
 var buttonArray = [];
 
-
 var userQuestions = [
-
-    {
-        question: "Commonly used data types Do not include:",
-        answers: {
-            0: "strings",
-            1: "booleans",
-            2: "alerts",
-            3: "numbers",
-        },
-    
-        correctAnswer: "alerts",
+  {
+    question: "Commonly used data types Do not include:",
+    answers: {
+      0: "strings",
+      1: "booleans",
+      2: "alerts",
+      3: "numbers",
     },
 
-    {
-        question: "The condition in an if / else statement enclosed with __________.",
-        answers: {
-            0: "quotes",
-            1: "curly brackets",
-            2: "parenthesis",
-            3: "square brackets",
-        },
-    
-        correctAnswer: "parenthesis",
+    correctAnswer: "alerts",
+  },
+
+  {
+    question:
+      "The condition in an if / else statement enclosed with __________.",
+    answers: {
+      0: "quotes",
+      1: "curly brackets",
+      2: "parenthesis",
+      3: "square brackets",
     },
 
-    {
-        question: "Arrays in JavaScript can be be used to store ________.",
-        answers: {
-            0: "numbers and strings",
-            1: "other arrays",
-            2: "booleans",
-            3: "all the above",
-        },
+    correctAnswer: "parenthesis",
+  },
 
-        correctAnswer: "all the above",
-
-    },
-    
-    {
-        question: "String values must be enclosed within ______ when being assigned to variables.",
-        answers: {
-            0: "commas",
-            1: "curly brackets",
-            2: "quotes",
-            3: "paranthesis",
-        },
-
-        correctAnswer: "quotes",
+  {
+    question: "Arrays in JavaScript can be be used to store ________.",
+    answers: {
+      0: "numbers and strings",
+      1: "other arrays",
+      2: "booleans",
+      3: "all the above",
     },
 
-    {
-        question: "A very useful tool used during developement and debugging for printing content to the debugger is:",
-        answers: {
-            0: "JavaScript",
-            1: "terminal/bash",
-            2: "for loops",
-            3: "console.log",
-        },
+    correctAnswer: "all the above",
+  },
 
-        correctAnswer: "console.log",
+  {
+    question:
+      "String values must be enclosed within ______ when being assigned to variables.",
+    answers: {
+      0: "commas",
+      1: "curly brackets",
+      2: "quotes",
+      3: "paranthesis",
     },
 
+    correctAnswer: "quotes",
+  },
+
+  {
+    question:
+      "A very useful tool used during developement and debugging for printing content to the debugger is:",
+    answers: {
+      0: "JavaScript",
+      1: "terminal/bash",
+      2: "for loops",
+      3: "console.log",
+    },
+
+    correctAnswer: "console.log",
+  },
 ];
 
 totalQuestions = userQuestions.length;
-var timer = document.querySelector("#timer"); 
+var timer = document.querySelector("#timer");
 var scoreLink = document.querySelector("scorelink");
-var principalContent = document.querySelector("#principal-content"); 
+var principalContent = document.querySelector("#principal-content");
 var questionSwitch = document.getElementById("question-change");
 var quizQuestion = document.querySelector("#quizquestion");
 var userSelection = document.querySelector("#userselection");
 var userScore = document.querySelector("#userscore");
 var userHighScore = document.querySelector("#userhighscores");
 var score = document.getElementById("scorelink");
-var startButton = document.getElementById("startquiz")
+var startButton = document.getElementById("startquiz");
 
 // starts test
 
 function start() {
-    timer();
-    question();
+  timer();
+  question();
 }
 
 function timer() {
-    
-    var interval = setInterval(function () {
-        
-        timeLeft--;
+  var interval = setInterval(function () {
+    timeLeft--;
 
-        timer.textContent = "Time : " + timeLeft;
-        
-        if (timeLeft <= 0 || (questionIndex > totalQuestions - 1)) {
-            
-            userSelection.style.display = "none";
-            quizQuestion.style.display = "none";
-            result();
-            clearInterval(interval);
-            timer.textContent = "";
-        }
+    timer.textContent = "Time : " + timeLeft;
 
-    }, 1000);
-
+    if (timeLeft <= 0 || questionIndex > totalQuestions - 1) {
+      userSelection.style.display = "none";
+      quizQuestion.style.display = "none";
+      result();
+      clearInterval(interval);
+      timer.textContent = "";
+    }
+  }, 1000);
 }
-        
-
 
 for (var i = 0; i < 4; i++) {
-    
-    var buttonDiv = document.createElement("div");
-    var selectionButtons = document.createElement("button");
-    selectionButtons.setAttribute("data-index", i);
-    selectionButtons.setAttribute("class", "btnn");
-    selectionArray.push(buttonDiv);
-    buttonArray.push(selectionButtons);
+  var buttonDiv = document.createElement("div");
+  var selectionButtons = document.createElement("button");
+  selectionButtons.setAttribute("data-index", i);
+  selectionButtons.setAttribute("class", "btnn");
+  selectionArray.push(buttonDiv);
+  buttonArray.push(selectionButtons);
 }
-
 
 // display of correct or incorrect answers
 
@@ -133,6 +123,5 @@ var correctIncorrect = document.createElement("p");
 correctIncorrect.setAttribute("class", "right-wrong");
 userSelection.appendChild(correctIncorrect);
 
-
 //start quiz button
-startButton.addEventListener("click",startChallenge);
+startButton.addEventListener("click", startChallenge);
